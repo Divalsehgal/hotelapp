e23e22900cd61cd124f1f2f95e34ffd6f17c2314
divalsehgal9 <dival.sehgal@nineleaps.com>
2020-12-23 23:39:12 +0530

    tg

--- src/components/__test__/Components.test.js
+++ src/components/__test__/Components.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAtrr } from "../../../utils/index";
+
 import Enzyme from "enzyme";
+
 import Adapter from "enzyme-adapter-react-16";
 import Banner from "../Banner";
 
