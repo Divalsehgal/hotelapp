ff7a1e1a36e9f03dcfa53c2434c0576a675d5b89
divalsehgal9 <dival.sehgal@nineleaps.com>
2020-12-23 19:18:53 +0530

    .

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,3 +1,5 @@
+set +x
+
 chmod +x test.sh
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
@@ -11,6 +13,7 @@ echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-
 set -x
 npm install --save-dev cross-env
 set +x
+#enables a mode of the shell where all executed commands are printed to the terminal. it's clearly used for debugging, which is a typical use case for set -x : printing every command as it is executed may help you to visualize the control flow of the script 
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
