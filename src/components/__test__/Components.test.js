9a763a4dc5e392b4cb321aa1d73eef76b960ef84
divalsehgal9 <dival.sehgal@nineleaps.com>
2021-01-12 21:28:35 +0530

    test cases

--- src/components/__test__/Components.test.js
+++ src/components/__test__/Components.test.js
@@ -1,33 +1,33 @@
-// import React from "react";
-// import { shallow } from "enzyme";
-// import { findByTestAtrr } from "../../../utils/index";
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAtrr } from "../../../utils/index";
 
-// import Enzyme from "enzyme";
+import Enzyme from "enzyme";
 
-// import Adapter from "enzyme-adapter-react-16";
-// import Banner from "../Banner";
-// import Hero from "../Hero";
-// import Loading from "../Loading";
-// import Title from "../Title";
+import Adapter from "enzyme-adapter-react-16";
+import Banner from "../Banner";
+import Hero from "../Hero";
+import Loading from "../Loading";
+import Title from "../Title";
 
-// Enzyme.configure({
-//   adapter: new Adapter(),
-// });
+Enzyme.configure({
+  adapter: new Adapter(),
+});
 
-// describe("Banner", () => {
-//   describe("Banner test", () => {
-//     let wrapper;
-//     beforeEach(() => {
-//       const props = {};
-//       wrapper = shallow(<Banner {...props} />);
-//     });
+describe("Banner", () => {
+  describe("Banner test", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {};
+      wrapper = shallow(<Banner {...props} />);
+    });
 
-//     it("should render banner", () => {
-//       const bannerComponent = findByTestAtrr(wrapper, "banner");
-//       expect(bannerComponent.length).toBe(1);
-//     });
-//   });
-// });
+    it("should render banner", () => {
+      const bannerComponent = findByTestAtrr(wrapper, "banner");
+      expect(bannerComponent.length).toBe(1);
+    });
+  });
+});
 
 // describe("Hero", () => {
 //   describe("Hero test", () => {
