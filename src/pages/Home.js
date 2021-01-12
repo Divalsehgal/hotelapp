f7f098346c3077c103936dd0e5d2f961102930f4
divalsehgal9 <dival.sehgal@nineleaps.com>
2021-01-12 11:24:12 +0530

    checking changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
   return (
     <>
       <Hero hero="defaultHero">
+        dival
         <Banner
           title="Luxurious Rooms"
           subtitle="Deluxe rooms starting at Rs.1500"
